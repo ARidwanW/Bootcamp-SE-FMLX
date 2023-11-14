be973783270ec7fb5730be359dcbe7323ac2df70
ARidwanW <alwandiaridwan@gmail.com>
2023-11-14 08:00:26 +0700

    Update: variable class & variable instance

--- Day-1/Program.cs
+++ Day-1/Program.cs
@@ -15,10 +15,12 @@ class Program
         pokari.Eat();
         pokari.isMale = true;
         Console.WriteLine(pokari.colour);
+        Console.WriteLine(pokari.age);
 
         Cat jiji = new Cat();
         jiji.colour = "black";
         Console.WriteLine(jiji.colour);
+        Console.WriteLine(jiji.age);
 
         Cat coki = new Cat();
         coki.colour = "white";
@@ -33,15 +35,18 @@ class Program
 class Cat    // class name : first general
 {
     // Variable / Field
+    // Class Variable
     public string colour;
     public int age;
     public bool isMale;
     public float weight;
 
     // Contructor
-    public Cat( string colour = "yellow")
+    public Cat( string colour = "yellow", int age = 0)
     {
+        // instance variable
         this.colour = colour;
+        this.age += 1;
     }
 
     // Method / Function
