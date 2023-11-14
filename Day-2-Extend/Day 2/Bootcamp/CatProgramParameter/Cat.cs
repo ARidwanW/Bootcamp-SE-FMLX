d4daa97dfcbdcc61c0693ab748fa7ee8e3318606
ARidwanW <alwandiaridwan@gmail.com>
2023-11-14 11:19:46 +0700

    Update: constructor, overloading, create object

--- Day-2-Extend/Day 2/Bootcamp/CatProgramParameter/Cat.cs
+++ Day-2-Extend/Day 2/Bootcamp/CatProgramParameter/Cat.cs
@@ -5,12 +5,26 @@ public class Cat
 	public string name;
 	public int age;
 	public float weight;
-	
-	public void Sleep() 
+
+	public Cat()
+	{
+		name = "cat";
+		age = 0;
+		weight = 0f;
+	}
+
+	public Cat(string name, int age, float weight)
+	{
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+	}
+
+	public void Sleep()
 	{
 		Console.WriteLine("Cat Sleep");
 	}
-	public void Eat(string makanan) 
+	public void Eat(string makanan)
 	{
 		Console.WriteLine("Cat eating " + makanan);
 		Console.WriteLine($"Cat Eating {makanan}");
--- Day-2-Extend/Day 2/Bootcamp/CatProgramParameter/Program.cs
+++ Day-2-Extend/Day 2/Bootcamp/CatProgramParameter/Program.cs
@@ -4,8 +4,18 @@ class Program
 {
 	static void Main() 
 	{
-		Cat cat = new Cat();
+		Cat cat = new Cat("pokari" , 3 , 3.0f);
 		cat.Sleep();
+
+		Cat cat1 = new Cat(age : 3, name : "yusuf", weight : 3.0f);
+
+		Cat cat2 = new Cat()
+		{
+			name = "jiji",
+			age = 4,
+			weight = 5.0f
+		};
+
 
 		string food = "whiskas";
 		cat.Eat(food);
