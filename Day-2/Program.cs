6d63828096dc3b81f3f2ab6635e90ed71ef2a22a
ARidwanW <alwandiaridwan@gmail.com>
2023-11-14 09:45:10 +0700

    Update: variable Cat

--- Day-2/Program.cs
+++ Day-2/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main()
     {
-        Cat pokari = new Cat("Pokari");
+        Cat pokari = new Cat("Pokari", "yellow", 2);
+        Cat jiji = new Cat("Jiji", "black", 1);
+
+        pokari.Eat();
+        pokari.Sleep();
+        pokari.Meow();
+
+        jiji.Eat();
+        jiji.Meow();
     }
 }
