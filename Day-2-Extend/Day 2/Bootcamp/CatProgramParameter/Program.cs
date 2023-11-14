9c40196eae1bccac1bd8dc2628ca6c12bd5cd01e
ARidwanW <alwandiaridwan@gmail.com>
2023-11-14 11:58:56 +0700

    Update: Initiate instance comment

--- Day-2-Extend/Day 2/Bootcamp/CatProgramParameter/Program.cs
+++ Day-2-Extend/Day 2/Bootcamp/CatProgramParameter/Program.cs
@@ -4,11 +4,14 @@ class Program
 {
 	static void Main() 
 	{
+		// Instantiate instance by its constructor
 		Cat cat = new Cat("pokari" , 3 , 3.0f);
 		cat.Sleep();
 
+		// Same, but different order
 		Cat cat1 = new Cat(age : 3, name : "yusuf", weight : 3.0f);
 
+		// Parameterless constructor, and fill the variable
 		Cat cat2 = new Cat()
 		{
 			name = "jiji",
