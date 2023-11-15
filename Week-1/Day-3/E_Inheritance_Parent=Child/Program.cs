bed029aef1689b7b7c5deb902688ab1886c1a370
ARidwanW <alwandiaridwan@gmail.com>
2023-11-15 10:45:22 +0700

    Update: output comment

--- Week-1/Day-3/E_Inheritance_Parent=Child/Program.cs
+++ Week-1/Day-3/E_Inheritance_Parent=Child/Program.cs
@@ -2,17 +2,18 @@
 class Program {
 	static void Main() {
 		Child c = new Child(1,2,3,4);
-		Parent p = c;
-		
-		//Parent p1 = new Child(1,2,3,4);
+		Parent p = c;   // its working
+		// Parent p1 = new Child(1,2,3,4);  // also working
 
-		//p.a.Dump(); ?
-		//p.b.Dump(); ?
-		//p.c.Dump(); ?
-		//p.d.Dump(); ?
+		p.a.Dump(); // output: 1
+		p.b.Dump(); // output: 2
+		/* p.c.Dump(); // output: error
+		p.d.Dump(); // output: error */
 		
-		//Parent p1 = new Parent(1,2);
-		//Child c1 = p1; ?
+		Parent p1 = new Parent(1,2);    // its working yeah classic
+
+        // this will error even when parent and child have the same number of parameter
+		/* Child c1 = p1;  // output: error CS0266: Cannot implicitly convert type 'Parent' to 'Child' */
 	}
 }
 class Jokowi {}
@@ -34,6 +35,10 @@ class Child : Parent
 		this.c = c;
 		this.d = d;
 	}
+    public Child(int a, int b)
+    {
+
+    }
 }
 
 //This is Extension Method
