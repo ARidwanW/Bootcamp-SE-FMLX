dd9c2dcba1c39abda615d722c68d8f26e93acf1f
ARidwanW <alwandiaridwan@gmail.com>
2023-11-15 09:46:03 +0700

    Add: Day-3 Project

---
+++ Week-1/Day-3/A_InheritanceTutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using Internal;
+using System;
+// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Cat cat = new Cat();
+        cat.Eat();
+        cat.Meow();
+        cat.colour = "yellow";
+        cat.age = 3;
+
+        Console.WriteLine(cat.colour);
+        Console.WriteLine(cat.age);
+    }
+}
