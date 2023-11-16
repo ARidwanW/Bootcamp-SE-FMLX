b3bc0abc33d88738c1ab06807e8fb2703fbdbef4
ARidwanW <alwandiaridwan@gmail.com>
2023-11-16 16:50:15 +0700

    Update: cleaning code tab

--- Week-1/Day-4-PR/Program.cs
+++ Week-1/Day-4-PR/Program.cs
@@ -1,35 +1,35 @@
 ﻿class Program
 {
-  static void Main()
-  {
-    Console.Write("Input Number: ");
-    int inputUser = Convert.ToInt32(Console.ReadLine());
-    
-    for (int i = 0 ; i <= inputUser; i++)
+    static void Main()
     {
-      if (i == 0)
-      {
-        Console.Write(i + " ");
-      }
-      else if (i % 3 == 0)
-      {
-        if ( i % 5 == 0)
+        Console.Write("Input Number: ");
+        int inputUser = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i <= inputUser; i++)
         {
-          Console.Write("foobar ");
+            if (i == 0)
+            {
+                Console.Write(i + " ");
+            }
+            else if (i % 3 == 0)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write("foobar ");
+                }
+                else
+                {
+                    Console.Write("foo ");
+                }
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("bar ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
         }
-        else
-        {
-          Console.Write("foo ");
-        }
-      }
-      else if (i % 5 == 0)
-      {
-        Console.Write("bar ");
-      }
-      else
-      {
-        Console.Write(i + " ");
-      }
     }
-  }
 }
