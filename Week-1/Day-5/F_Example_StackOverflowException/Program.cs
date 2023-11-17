8c35a1f06f09312c7b7e17b138df1746420beceb
ARidwanW <alwandiaridwan@gmail.com>
2023-11-17 09:26:48 +0700

    Update: Example stackoverflowexception

--- Week-1/Day-5/F_Example_StackOverflowException/Program.cs
+++ Week-1/Day-5/F_Example_StackOverflowException/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Main()
+{
+	Run();
+}
+static void Run() {
+	Run();
+}
+//StackOverFlowException
+
+// so bassically stack memory have 2 - 8 MB
+// but heap memory can save 200GB
+
+// * so when you run a method of itself for unlimited of call
+// * it will return StackOverFlowException and crash
