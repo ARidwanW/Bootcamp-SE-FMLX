99a5312cdec0432d991b380c4afdc0e6248655c5
ARidwanW <alwandiaridwan@gmail.com>
2023-11-24 05:47:45 +0700

    Update: some test

--- FooBar/Program.cs
+++ FooBar/Program.cs
@@ -33,11 +33,11 @@ class Program
 
         Console.WriteLine( foobarv2.AddCondition(3, "foo"));    // ?? output: True
         foobarv2.AddCondition(5, "bar");
-        foobarv2.AddCondition(7, "roo");
+        // foobarv2.AddCondition(7, "roo");
         foobarv2.UpdateCondition(5, "roo");
         Console.WriteLine(foobarv2.AddCondition(7, "kiwkiw"));  // ?? output: False
         foobarv2.RemoveCondition(5);
-        foobarv2.AddCondition(condition);
+        // foobarv2.AddCondition(condition);
         // foobarv2.AddIterator(2);            // ?? output: 1 2
         // foobarv2.AddIterator(1, 2, 3, 4);   // ?? output: 1 2 1 2 3 4
         // foobarv2.UpdateIterator(0, 2);      // ?? output: 2 2 1 2 3 4
