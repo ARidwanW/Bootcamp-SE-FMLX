b1ce22baf851e997a2c580eeb685d13ca9ba9953
ARidwanW <alwandiaridwan@gmail.com>
2023-12-04 20:16:13 +0700

    Add: class MSPlayer

---
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Player/MSPlayer.cs
@@ -0,0 +1,18 @@
+namespace MarvelSnapProject.Component.Player;
+
+public class MSPlayer : IPlayer
+{
+    public int Id {get; private set;}
+    public string Name {get; private set;}
+
+    /// <summary>
+    /// Class MSPlayer implement IPlayer
+    /// </summary>
+    /// <param name="id">Assign Id of player</param>
+    /// <param name="name">Assign Name of player</param>
+    public MSPlayer(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
