c4365a3ea9360c7be44d15ed47ef2a732959f3ae
ARidwanW <alwandiaridwan@gmail.com>
2023-12-05 17:49:00 +0700

    Update: property & DoAbility()

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/AbstractCard.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/AbstractCard.cs
@@ -1,22 +1,68 @@
-using System.Globalization;
+using MarvelSnapProject.Enum;
 
 namespace MarvelSnapProject.Component.Card;
 
 public abstract class AbstractCard
 {
+    /// <summary>
+    /// Readonly property Id of Card
+    /// </summary>
+    /// <value>int</value>
     public int Id { get; private set; }
+
+    /// <summary>
+    /// Readonly property Name of Card
+    /// </summary>
+    /// <value>string</value>
     public string Name { get; private set; }
+
+    /// <summary>
+    /// Readonly property Description of Card
+    /// </summary>
+    /// <value>string</value>
     public string Description { get; private set; }
+
+    /// <summary>
+    /// Readonly property Cost of Card
+    /// </summary>
+    /// <value>int</value>
     public int Cost { get; private set; }
+
+    /// <summary>
+    /// Readonly property Power of Card
+    /// </summary>
+    /// <value>int</value>
     public int Power { get; private set; }
 
-    public AbstractCard(int id, string name, string description, int cost, int power)
+    /// <summary>
+    /// Readonly property CardTimeAbility of Card
+    /// </summary>
+    /// <value>CardTimeAbility</value>
+    public CardTimeAbility CardTimeAbility { get; private set; }
+
+    /// <summary>
+    /// Readonly property CardAbility of Card
+    /// </summary>
+    /// <value>CardAbility</value>
+    public CardAbility CardAbility { get; private set; }
+
+    /// <summary>
+    /// Public property CardStatus of Card
+    /// </summary>
+    /// <value></value>
+    public CardStatus CardStatus { get; set; }
+
+    public AbstractCard(int id, string name, string description, int cost, int power, CardTimeAbility cardTimeAbility, CardAbility cardAbility)
     {
         Id = id;
         Name = name;
         Description = description;
         Cost = cost;
         Power = power;
+        CardTimeAbility = cardTimeAbility;
+        CardAbility = cardAbility;
     }
+
+    public abstract bool DoAbility();
 
 }
