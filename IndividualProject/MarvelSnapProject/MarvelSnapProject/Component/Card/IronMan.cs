fe7ccc3d68f81f4732c54281b0422c9e6456a05c
ARidwanW <alwandiaridwan@gmail.com>
2023-12-06 11:11:40 +0700

    Update: example card

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/IronMan.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/IronMan.cs
@@ -1,16 +1,22 @@
 using MarvelSnapProject.Component.Player;
 using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Enum;
 
 namespace MarvelSnapProject.Component.Card;
 
 public class IronMan : AbstractCard
 {
-    public IronMan() : base(1, "Iron Man", "Manusia Logam", 5, 0, Enum.CardTimeAbility.OnGoing, Enum.CardAbility.OnGoing)
+    public IronMan() : base(1, "Iron Man", "Manusia Logam", 5, 0, CardAbility.OnGoing, true, false)
     {
-        
     }
-    public override bool DoAbility(GameController game, IPlayer player, AbstractLocation location)
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
     {
-        return true;
+        throw new NotImplementedException();
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
     }
 }
