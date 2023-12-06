6f1f1821dc0896939cf213f32f9368818b5579d2
ARidwanW <alwandiaridwan@gmail.com>
2023-12-06 11:29:47 +0700

    Update: add documentation

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/AbstractCard.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/AbstractCard.cs
@@ -41,12 +41,37 @@ public abstract class AbstractCard
     /// </summary>
     /// <value>CardAbility</value>
     public CardAbility CardAbility { get; private set; }
-    private CardStatus _cardStatus;
-    public bool _isOnGoing { get; }
-    public bool _isOnReveal { get; }
 
-    public AbstractCard(int id, string name, string description, 
-                        int cost, int power, CardAbility cardAbility, 
+    /// <summary>
+    /// Private variable Status of Card
+    /// </summary>
+    private CardStatus _cardStatus;
+
+    /// <summary>
+    /// Readonly Property OnGoing ability of Card
+    /// </summary>
+    /// <value>bool</value>
+    public bool _isOnGoing { get; private set; }
+
+    /// <summary>
+    /// Readonly Property OnReveal ability of Card
+    /// </summary>
+    /// <value>bool</value>
+    public bool _isOnReveal { get; private set; }
+
+    /// <summary>
+    /// Abstract Class of Card
+    /// </summary>
+    /// <param name="id">Id of Card</param>
+    /// <param name="name">Name of Card</param>
+    /// <param name="description">Description of Card Ability</param>
+    /// <param name="cost">Cost or Energy of Card</param>
+    /// <param name="power">Power or Point of Card</param>
+    /// <param name="cardAbility">Ability type of Card</param>
+    /// <param name="isOnGoing">Boolean: true if ability is OnGoing type</param>
+    /// <param name="isOnReveal">Boolean: true if ability is OnReveal type</param>
+    public AbstractCard(int id, string name, string description,
+                        int cost, int power, CardAbility cardAbility,
                         bool isOnGoing = false, bool isOnReveal = false)
     {
         Id = id;
@@ -59,19 +84,42 @@ public abstract class AbstractCard
         _isOnReveal = isOnReveal;
     }
 
-    // public abstract bool DoAbility(GameController game, IPlayer player, AbstractLocation location);
+    /// <summary>
+    /// Abstract method for special ability on going.
+    /// </summary>
+    /// <param name="game">Game Controller</param>
+    /// <param name="player">IPlayer implementation of players</param>
+    /// <param name="location">AbstractLocation implementation of locations</param>
+    /// <returns>true: if card has on going special ability</returns>
     public abstract bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location);
+
+
+    /// <summary>
+    /// Abstract method for special ability on reveal.
+    /// </summary>
+    /// <param name="game">Game Controller</param>
+    /// <param name="player">IPlayer implementation of players</param>
+    /// <param name="location">AbstractLocation implementation of locations</param>
+    /// <returns>true: if card has on reveal special ability</returns>
     public abstract bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location);
 
+    /// <summary>
+    /// Getter status of Card
+    /// </summary>
+    /// <returns>CardStatus</returns>
     public CardStatus GetCardStatus()
     {
         return _cardStatus;
     }
 
+    /// <summary>
+    /// Setter status of Card
+    /// </summary>
+    /// <param name="status">Changed status</param>
+    /// <returns>true: if card status successfully changed</returns>
     public bool SetCardStatus(CardStatus status)
     {
         _cardStatus = status;
         return true;
     }
-
 }
