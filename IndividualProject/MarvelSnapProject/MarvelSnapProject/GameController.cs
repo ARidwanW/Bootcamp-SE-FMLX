94148e64a5c289a5e8f852c30bae15c028566c7c
ARidwanW <alwandiaridwan@gmail.com>
2023-12-07 06:47:10 +0700

    Update: add some field of GameController

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/GameController.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/GameController.cs
@@ -1,5 +1,20 @@
+using MarvelSnapProject.Component.Card;
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+using NLog;
+
 namespace MarvelSnapProject;
 
 public class GameController
 {
+    private static readonly Logger logger;
+    private GameStatus _gameStatus;
+    private int _round;
+    private Dictionary<IPlayer, PlayerInfo> _players;
+    private Dictionary<AbstractLocation, LocationInfo> _locations;
+    private List<AbstractLocation> _allLocations;
+    private List<AbstractCard> _allCards;
+
+
 }
