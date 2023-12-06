9aa633b0e1b1a60092562c318298dbc8d245d2da
ARidwanW <alwandiaridwan@gmail.com>
2023-12-06 11:10:30 +0700

    Update: isOnGoing isOnReveal var & abstract method

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/AbstractCard.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/AbstractCard.cs
@@ -37,30 +37,31 @@ public abstract class AbstractCard
     public int Power { get; private set; }
 
     /// <summary>
-    /// Readonly property TimeAbility of Card
-    /// </summary>
-    /// <value>CardTimeAbility</value>
-    public CardTimeAbility CardTimeAbility { get; private set; }
-
-    /// <summary>
     /// Readonly property Ability of Card
     /// </summary>
     /// <value>CardAbility</value>
     public CardAbility CardAbility { get; private set; }
     private CardStatus _cardStatus;
+    public bool _isOnGoing { get; }
+    public bool _isOnReveal { get; }
 
-    public AbstractCard(int id, string name, string description, int cost, int power, CardTimeAbility cardTimeAbility, CardAbility cardAbility)
+    public AbstractCard(int id, string name, string description, 
+                        int cost, int power, CardAbility cardAbility, 
+                        bool isOnGoing = false, bool isOnReveal = false)
     {
         Id = id;
         Name = name;
         Description = description;
         Cost = cost;
         Power = power;
-        CardTimeAbility = cardTimeAbility;
         CardAbility = cardAbility;
+        _isOnGoing = isOnGoing;
+        _isOnReveal = isOnReveal;
     }
 
-    public abstract bool DoAbility(GameController game, IPlayer player, AbstractLocation location);
+    // public abstract bool DoAbility(GameController game, IPlayer player, AbstractLocation location);
+    public abstract bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location);
+    public abstract bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location);
 
     public CardStatus GetCardStatus()
     {
