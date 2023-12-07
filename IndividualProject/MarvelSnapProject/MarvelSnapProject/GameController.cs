d68674fae6daba0e123ff2d33d3aca28eda542bf
ARidwanW <alwandiaridwan@gmail.com>
2023-12-07 08:00:15 +0700

    Update: add winner var & contructor

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/GameController.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/GameController.cs
@@ -8,13 +8,21 @@ namespace MarvelSnapProject;
 
 public class GameController
 {
-    private static readonly Logger logger;
+    private readonly Logger _logger;
     private GameStatus _gameStatus;
     private int _round;
     private Dictionary<IPlayer, PlayerInfo> _players;
     private Dictionary<AbstractLocation, LocationInfo> _locations;
     private List<AbstractLocation> _allLocations;
     private List<AbstractCard> _allCards;
+    private IPlayer _winner;
+
+    public GameController(Logger log)
+    {
+        _logger = log;
+        _gameStatus = GameStatus.None;
+        _round = 0;
+    }
 
 
 }
