0299683c4cd8036b4d3467874d095c1ad6359d31
ARidwanW <alwandiaridwan@gmail.com>
2023-12-07 10:45:46 +0700

    Update: some changes

--- Week-4/Day-4/ZZ_AdditionalMaterial/E_HotReload/Program.cs
+++ Week-4/Day-4/ZZ_AdditionalMaterial/E_HotReload/Program.cs
@@ -1,7 +1,10 @@
 ﻿//Use HotReload on Debugger --> dotnet watch
-
+int i = 0;
 while (true)
 {
-	Console.WriteLine("yanto");
-	await Task.Delay(1);
+	Console.WriteLine("beto");
+	Console.WriteLine("sate");
+	i++;
+	Console.WriteLine(i);
+	await Task.Delay(1000);
 }
