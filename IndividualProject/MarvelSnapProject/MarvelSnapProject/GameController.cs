b4d84eb1f10047c6881a80e98faea46349148939
ARidwanW <alwandiaridwan@gmail.com>
2023-12-07 20:42:04 +0700

    Update: reduce warning

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/GameController.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/GameController.cs
@@ -8,35 +8,45 @@ namespace MarvelSnapProject;
 
 public class GameController
 {
-    private readonly Logger _logger;
+    private readonly Logger? _logger;
     private GameStatus _gameStatus;
     private int _round;
-    private Dictionary<IPlayer, PlayerInfo> _players;
-    private Dictionary<AbstractLocation, LocationInfo> _locations;
-    private List<AbstractLocation> _allLocations;
-    private List<AbstractCard> _allCards;
-    private IPlayer _winner;
-    private Action<AbstractCard, CardStatus> OnCardStatusUpdate;
-    private Action<AbstractLocation, LocationStatus> OnLocationStatusUpdate;
-    private Action<AbstractLocation, LocationInfo> OnLocationUpdate;
-    private Action<IPlayer, PlayerStatus> OnPlayerStatusUpdate;
-    private Action<IPlayer, PlayerInfo> OnPlayerUpdate;
-    private Action<GameController, IPlayer, AbstractLocation> OnRevealCardAbilityCall;        // invoke every round, chek apakah ada sub, jika iya bakal di invoke dan chek apakah roundnya sudah selanjutnya
-    private Action<GameController> OnRevealLocationAbilityCall;
-    private Action<GameController, IPlayer, AbstractLocation> OnGoingCardAbilityCall;
-    private Action<GameController> OnGoingLocationAbilityCall;
+    private Dictionary<IPlayer, PlayerInfo>? _players;
+    private Dictionary<AbstractLocation, LocationInfo>? _locations;
+    private List<AbstractLocation>? _allLocations;
+    private List<AbstractCard>? _allCards;
+    private IPlayer? _winner;
+    private Action<AbstractCard, CardStatus>? OnCardStatusUpdate;
+    private Action<AbstractLocation, LocationStatus>? OnLocationStatusUpdate;
+    private Action<AbstractLocation, LocationInfo>? OnLocationUpdate;
+    private Action<IPlayer, PlayerStatus>? OnPlayerStatusUpdate;
+    private Action<IPlayer, PlayerInfo>? OnPlayerUpdate;
+    private Action<GameController, IPlayer, AbstractLocation>? OnRevealCardAbilityCall;        // invoke every round, chek apakah ada sub, jika iya bakal di invoke dan chek apakah roundnya sudah selanjutnya
+    private Action<GameController>? OnRevealLocationAbilityCall;
+    private Action<GameController, IPlayer, AbstractLocation>? OnGoingCardAbilityCall;
+    private Action<GameController>? OnGoingLocationAbilityCall;
 
 
-    public GameController(Logger log)
+    public GameController(Logger? log = null)
     {
         _logger = log;
         _gameStatus = GameStatus.None;
+        _players = new Dictionary<IPlayer, PlayerInfo>();
+        _locations = new Dictionary<AbstractLocation, LocationInfo>();
+        _allLocations = new List<AbstractLocation>();
+        _allCards = new List<AbstractCard>();
         _round = 0;
     }
 
     public GameStatus GetCurrentGameStatus()
     {
         return _gameStatus;
+    }
+
+    public bool SetGameStatus(GameStatus status)
+    {
+        _gameStatus = status;
+        return true;
     }
 
     public int CheckCurrentRound()
@@ -46,6 +56,7 @@ public class GameController
 
     public bool NextRound()
     {
+        _round += 1;
         return true;
     }
 
@@ -61,11 +72,7 @@ public class GameController
 
     public List<IPlayer> GetAllPlayers()
     {
-        List<IPlayer> players = new List<IPlayer>();
-        foreach(var player in _players)
-        {
-            players.Add(player.Key);
-        }
+        List<IPlayer> players = _players.Keys.ToList();
         return players;
     }
 
