2c6a7020f7c4e3b466bd65ca26254ce8ab07059b
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 13:59:09 +0700

    Update: init implement Hawkeye.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Howkeye.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Howkeye.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class Howkeye
+public class Howkeye : AbstractCard
 {
+    public Howkeye() : base(3, "Hawkeye", "On Reveal: if you play a card at this location next turn, +3 power.", 
+                            1, 1, CardAbility.OnReveal, CardStatus.None, false, true)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return true;
+    }
 }
