c6317719940b867522d49ba9136395d14fec1c3e
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 14:02:37 +0700

    Update: init implementation JessicaJones.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/JessicaJones.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/JessicaJones.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class JessicaJones
+public class JessicaJones : AbstractCard
 {
+    public JessicaJones() : base(6, "Jessica Jones", "On Reveal: if you don't play a card at this location next turn, +4 Power.", 
+                                4, 5, CardAbility.OnReveal, CardStatus.None, false, true)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return true;
+    }
 }
