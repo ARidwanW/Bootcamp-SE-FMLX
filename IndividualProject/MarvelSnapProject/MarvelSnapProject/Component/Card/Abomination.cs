8fbfbad03fa6d55855aee28662b941443bf01eae
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 10:07:13 +0700

    Update: implementation Abomination.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Abomination.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Abomination.cs
@@ -1,5 +1,22 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class Abomination
+public class Abomination : AbstractCard
 {
+    public Abomination() : base(1, "Abomination", "Foolish rabble! You are beneath me!", 5, 9, CardAbility.None, CardStatus.None, false, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
