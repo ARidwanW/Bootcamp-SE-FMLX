6caf411493317f8374b1ad628c71cad5e403882f
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 10:14:17 +0700

    Update: implementataion Hulk.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Hulk.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Hulk.cs
@@ -1,5 +1,22 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class Hulk
+public class Hulk : AbstractCard
 {
+    public Hulk() : base(4, "Hulk", "HULK SMASH!", 6, 12, CardAbility.None, CardStatus.None, false, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
