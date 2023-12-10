6560057dccb1d6a53c77ef614a3028188c7b49a1
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 14:05:07 +0700

    Update: init implementation The Punisher.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/ThePunisher.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/ThePunisher.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class ThePunisher
+public class ThePunisher : AbstractCard
 {
+    public ThePunisher() : base(14, "The Punisher", "On Going: +1 Power for each opposing card at this location.", 
+                                3, 2, CardAbility.OnGoing, CardStatus.None, true, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return true;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
