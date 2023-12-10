cd48d909f4bd46906f24a581863ba4b1bb0411dd
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 22:58:26 +0700

    Update: simple test assign

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Program.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MarvelSnapProject;
 using MarvelSnapProject.Component.Card;
 using MarvelSnapProject.Component.Location;
 using MarvelSnapProject.Component.Player;
@@ -7,25 +8,62 @@ public partial class Program
 {
     private static void Main(string[] args)
     {
-        PlayerInfo playerInfo = new PlayerInfo();
-        IronMan ironMan = new IronMan();
-        Console.WriteLine(playerInfo.AssignCardToDeck(ironMan));
-        var deck = playerInfo.GetDeck();
-        foreach(var card in deck)
+        //* test to get item name from list
+        // PlayerInfo playerInfo = new PlayerInfo();
+        // IronMan ironMan = new IronMan();
+        // Console.WriteLine(playerInfo.AssignCardToDeck(ironMan));
+        // var deck = playerInfo.GetDeck();
+        // foreach(var card in deck)
+        // {
+        //     Console.WriteLine(card.Name);
+        // }
+        // Console.WriteLine(playerInfo.AssignCardToHand(ironMan));
+        // Console.WriteLine(playerInfo.RetrieveCardFromDeck(ironMan));
+        // var hand = playerInfo.GetHandCards();
+        // foreach(var card in hand)
+        // {
+        //     Console.WriteLine(card.Name);
+        // }
+        // deck = playerInfo.GetDeck();
+        // foreach(var card in deck)
+        // {
+        //     Console.WriteLine(card.Name);
+        // }
+
+        //* test GetLocation(AbstractLocation) to get item from List<AbstractLocation> in GameController
+        // GameController game = new();
+        // Asgard asgard = new();
+        // Atlantis atlantis = new();
+        // Attilan attilan = new();
+        // Flooded flooded = new();
+        // game.AssignLocation(asgard, atlantis, attilan);
+        // var location = game.GetLocation(flooded);
+        // var locations = game.GetAllLocations();
+        // foreach(var item in locations)
+        // {
+        //     Console.WriteLine(item);
+        // }
+        // Console.WriteLine(location.Name);
+
+        //* simple test
+        GameController game = new();
+        MSPlayer Wawan = new(1, "Wawan");
+        Hawkeye hawkeye = new();
+        var hawkeyeCard = hawkeye.Clone();
+        Abomination abomination = new();
+        game.AssignPlayer(Wawan);
+        game.AssignCardToPlayerDeck(Wawan,hawkeyeCard);
+        var wawanDeck = game.GetPlayerDeck(Wawan);
+        foreach(var card in wawanDeck)
         {
-            Console.WriteLine(card.Name);
+            Console.WriteLine(card.Name + card.GetCardStatus());
         }
-        Console.WriteLine(playerInfo.AssignCardToHand(ironMan));
-        Console.WriteLine(playerInfo.RetrieveCardFromDeck(ironMan));
-        var hand = playerInfo.GetHandCards();
-        foreach(var card in hand)
+        var assignCardToWawan = game.AssignCardToPlayerHand(Wawan, hawkeyeCard);
+        Console.WriteLine(assignCardToWawan);
+        foreach(var card in game.GetPlayerHand(Wawan))
         {
-            Console.WriteLine(card.Name);
+            Console.WriteLine(card.Name + card.GetCardStatus());
         }
-        deck = playerInfo.GetDeck();
-        foreach(var card in deck)
-        {
-            Console.WriteLine(card.Name);
-        }
+
     }
 }
