aa84e590b2ff9ad20afc021603936a31543b5037
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 10:31:52 +0700

    Update: implementation MistyKnight.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/MistyKnight.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/MistyKnight.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class MistyKnight
+public class MistyKnight : AbstractCard
 {
+    public MistyKnight() : base(9, "Misty Knight", "We've got to save this city.", 
+                                1, 2, CardAbility.None, CardStatus.None, false, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
