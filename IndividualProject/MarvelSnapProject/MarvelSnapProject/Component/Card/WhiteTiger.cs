e57dea7116a500c00f0ec0ddded7790f1f71d735
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 14:05:31 +0700

    Update: init implementation WhiteTiger.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/WhiteTiger.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/WhiteTiger.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class WhiteTiger
+public class WhiteTiger : AbstractCard
 {
+    public WhiteTiger() : base(16, "White Tiger", "On Reveal: Add a 8-Power Tiger to another location", 
+                                5, 1, CardAbility.OnReveal, CardStatus.None, false, true)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return true;
+    }
 }
