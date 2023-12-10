0d40e519c08167521bc955a64b2a4a07547fa843
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 14:05:50 +0700

    Update: init implementation Asgard.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/Asgard.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/Asgard.cs
@@ -2,11 +2,18 @@ namespace MarvelSnapProject.Component.Location;
 
 public class Asgard : AbstractLocation
 {
-    public Asgard() : base(1, "Asgard", "tempat para dewa", Enum.LocationAbility.Asgard)
+
+    public Asgard() : base(1, "Asgard", "After turn 4, whoever is winning here draws 2.", Enum.LocationAbility.Asgard, Enum.LocationStatus.Hidden, false, true)
     {
         
     }
-    public override bool DoAbility(GameController game)
+
+    public override bool SpecialAbilityOnGoing(GameController game)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game)
     {
         return true;
     }
