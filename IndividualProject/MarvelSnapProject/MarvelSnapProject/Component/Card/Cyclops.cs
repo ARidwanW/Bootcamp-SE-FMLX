7e072aa157f773de444eb869492d2cd31f08680c
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 10:04:11 +0700

    Update: implementation Cyclops.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Cyclops.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Cyclops.cs
@@ -1,5 +1,22 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class Cyclops
+public class Cyclops : AbstractCard
 {
+    public Cyclops() : base(2, "Cyclops", "Lets move, X-men.", 3, 4, CardAbility.None, CardStatus.None, false, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
