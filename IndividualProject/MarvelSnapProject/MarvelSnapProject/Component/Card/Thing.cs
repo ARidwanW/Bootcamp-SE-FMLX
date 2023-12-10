145d47ea2c63148acc6bf9abe8c274f867bbf385
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 10:49:02 +0700

    Update: implementation Thing.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Thing.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Thing.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class Thing
+public class Thing : AbstractCard
 {
+    public Thing() : base(15, "Thing", "It's clobberin' time!", 
+                        4, 6, CardAbility.None, CardStatus.None, false, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
