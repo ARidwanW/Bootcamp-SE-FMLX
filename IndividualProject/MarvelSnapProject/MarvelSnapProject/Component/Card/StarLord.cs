d0e6df4839aa0e308e967385de57ce98c01a6839
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 14:04:21 +0700

    Update: init implementation StarLord.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/StarLord.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/StarLord.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class StarLord
+public class StarLord : AbstractCard
 {
+    public StarLord() : base(13, "Star Lord", "On Reveal: If your opponent played a card here this turn, +3 Power.", 
+                            2, 2, CardAbility.OnReveal, CardStatus.None, false, true)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return true;
+    }
 }
