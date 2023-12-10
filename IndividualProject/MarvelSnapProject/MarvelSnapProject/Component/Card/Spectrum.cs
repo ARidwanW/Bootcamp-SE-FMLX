efe8d97ec55c12371b39bafe80e384b956635ffb
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 14:03:51 +0700

    Update: init implementation Spectrum.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Spectrum.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Spectrum.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class Spectrum
+public class Spectrum : AbstractCard
 {
+    public Spectrum() : base(12, "Spectrum", "On Reveal: Give your Ongoing cards +2 Power.", 
+                            6, 7, CardAbility.OnReveal, CardStatus.None, false, true)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return true;
+    }
 }
