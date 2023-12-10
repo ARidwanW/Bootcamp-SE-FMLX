1e6b6b52ff2cda5ed49f7b615096bef46029287a
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 14:03:28 +0700

    Update: init implementation MisterFantastic.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/MisterFantastic.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/MisterFantastic.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class MisterFantastic
+public class MisterFantastic : AbstractCard
 {
+    public MisterFantastic() : base(8, "Mister Fantastic", "On Going: Adjacent locations have +2 Power.", 
+                                    3, 2, CardAbility.OnGoing, CardStatus.None, true, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return true;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
