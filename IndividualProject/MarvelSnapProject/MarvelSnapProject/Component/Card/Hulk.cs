ad1baa9883d43736411e398593915a5d3aafe1c8
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 10:14:56 +0700

    Format: base parameter 1 line to 2 lines

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Hulk.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Hulk.cs
@@ -6,7 +6,8 @@ namespace MarvelSnapProject.Component.Card;
 
 public class Hulk : AbstractCard
 {
-    public Hulk() : base(4, "Hulk", "HULK SMASH!", 6, 12, CardAbility.None, CardStatus.None, false, false)
+    public Hulk() : base(4, "Hulk", "HULK SMASH!", 
+                        6, 12, CardAbility.None, CardStatus.None, false, false)
     {
     }
 
