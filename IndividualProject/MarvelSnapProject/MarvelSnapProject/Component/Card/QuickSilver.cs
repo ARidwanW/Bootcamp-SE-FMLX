7affc120696b732d5b758e792e761d7bbec7adc4
ARidwanW <alwandiaridwan@gmail.com>
2023-12-10 10:34:29 +0700

    Update: implementation QuickSilver.cs

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/QuickSilver.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/QuickSilver.cs
@@ -1,5 +1,23 @@
+using MarvelSnapProject.Component.Location;
+using MarvelSnapProject.Component.Player;
+using MarvelSnapProject.Enum;
+
 namespace MarvelSnapProject.Component.Card;
 
-public class QuickSilver
+public class QuickSilver : AbstractCard
 {
+    public QuickSilver() : base(10, "Quick Silver", "Starts in your opening hand", 
+                                1, 2, CardAbility.None, CardStatus.None, false, false)
+    {
+    }
+
+    public override bool SpecialAbilityOnGoing(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
+
+    public override bool SpecialAbilityOnReveal(GameController game, IPlayer player, AbstractLocation location)
+    {
+        return false;
+    }
 }
