778511f85a17b83df4c6888dc4cd309fd11c5193
ARidwanW <alwandiaridwan@gmail.com>
2023-12-12 05:54:37 +0700

    Update: implemen ability Spectrum

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Spectrum.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/Spectrum.cs
@@ -6,7 +6,10 @@ namespace MarvelSnapProject.Component.Card;
 
 public class Spectrum : AbstractCard
 {
-    public Spectrum() : base(12, "Spectrum", "On Reveal: Give your Ongoing cards +2 Power.", 
+    private IPlayer _deployer;
+    private int _roundDeployed;
+
+    public Spectrum() : base(12, "Spectrum", "On Reveal: Give your Ongoing cards +2 Power.",
                             6, 7, CardAbility.OnReveal, CardStatus.None, false, true)
     {
     }
@@ -23,6 +26,72 @@ public class Spectrum : AbstractCard
 
     public override bool SpecialAbilityOnReveal(GameController game)
     {
+        if (IsDeployed())
+        {
+            //* NextRound --> invoke --> round + 1
+            if (game.GetCurrentRound() != _roundDeployed + 1)
+            {
+                return false;
+            }
+
+            var allDeployedLocations = game.GetAllDeployedLocations();
+            List<AbstractCard> playerCards = new List<AbstractCard>();
+            foreach (var location in allDeployedLocations)
+            {
+                playerCards = location.GetPlayerCards(_deployer);
+            }
+
+            foreach (var card in playerCards)
+            {
+                if (card.Name != this.Name)
+                {
+                    card.SetPower(card.GetPower() + 2);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public override bool DeployCard(GameController game, IPlayer player, AbstractLocation location)
+    {
+        if (!IsDeployed() && GetCardStatus() == CardStatus.OnHand)
+        {
+            SetCardStatus(CardStatus.OnLocation);
+            SetRoundDeployed(game.GetCurrentRound());
+            RegisterSpecialAbilityOnReveal(game);
+            return true;
+        }
+        return false;
+    }
+
+    public IPlayer GetDeployer()
+    {
+        return _deployer;
+    }
+
+    public bool SetDeployer(IPlayer player)
+    {
+        _deployer = player;
         return true;
+    }
+
+    public int GetRoundDeployed()
+    {
+        return _roundDeployed;
+    }
+
+    public bool SetRoundDeployed(int round)
+    {
+        _roundDeployed = round;
+        return true;
+    }
+
+    public void RegisterSpecialAbilityOnReveal(GameController game)
+    {
+        if (IsDeployed())
+        {
+            game.OnRevealCardAbilityCall += SpecialAbilityOnReveal;
+        }
     }
 }
