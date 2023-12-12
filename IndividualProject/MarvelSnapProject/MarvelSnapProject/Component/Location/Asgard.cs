197c428b7f53455755848475ea46fc2c81341779
ARidwanW <alwandiaridwan@gmail.com>
2023-12-12 12:00:13 +0700

    Update: implement location ability Asgard

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/Asgard.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/Asgard.cs
@@ -1,13 +1,13 @@
+using MarvelSnapProject.Component.Player;
 using MarvelSnapProject.Enum;
 
 namespace MarvelSnapProject.Component.Location;
 
 public class Asgard : AbstractLocation
 {
-
     public Asgard() : base(1, "Asgard", "After turn 4, whoever is winning here draws 2.", LocationAbility.Asgard, LocationStatus.Hidden, false, true)
     {
-        
+
     }
 
     public override bool SpecialAbilityOnGoing(GameController game)
@@ -17,6 +17,51 @@ public class Asgard : AbstractLocation
 
     public override bool SpecialAbilityOnReveal(GameController game)
     {
+        //* NextRound --> invoke --> round + 1
+        if (game.GetCurrentRound() != 4)
+        {
+            return false;
+        }
+
+        var allPlayersPower = GetAllPlayersPower();
+        List<IPlayer> locationWinners = new List<IPlayer>();
+        int maxPower = 0;
+
+        if (allPlayersPower.Count < 0)
+        {
+            return false;
+        }
+
+        foreach (var playerPower in allPlayersPower)
+        {
+            if (playerPower.Value > maxPower)
+            {
+                maxPower = playerPower.Value;
+            }
+        }
+
+        foreach (var playerPower in allPlayersPower)
+        {
+            if (playerPower.Value == maxPower)
+            {
+                locationWinners.Add(playerPower.Key);
+            }
+        }
+
+        var randomCard = game.GetShuffleCard();
+
+        foreach(var winner in locationWinners)
+        {
+            game.AssignCardToPlayerHand(winner, randomCard);
+        }
         return true;
+    }
+
+    public void RegisterSpecialAbilityOnReveal(GameController game)
+    {
+        if (this.GetLocationStatus() == LocationStatus.Revealed)
+        {
+            game.OnRevealLocationAbilityCall += SpecialAbilityOnReveal;
+        }
     }
 }
