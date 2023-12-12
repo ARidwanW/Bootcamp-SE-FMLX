651a46c4e2a3f6faf79cdf1c62fbf1185118d3ae
ARidwanW <alwandiaridwan@gmail.com>
2023-12-12 15:26:06 +0700

    Update: implement locatino ability Atlantis

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/Atlantis.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/Atlantis.cs
@@ -4,17 +4,36 @@ namespace MarvelSnapProject.Component.Location;
 
 public class Atlantis : AbstractLocation
 {
-    public Atlantis() : base(2, "Atlantis", "If you only have one card here, it has +5 Power.", LocationAbility.Atlantis, LocationStatus.Hidden, true, true)
+    public Atlantis() : base(2, "Atlantis", "If you only have one card here, it has +5 Power.", 
+    LocationAbility.Atlantis, LocationStatus.Hidden, true, false)
     {
     }
 
     public override bool SpecialAbilityOnGoing(GameController game)
     {
+        foreach(var playersCards in GetAllPlayersCards())
+        {
+            if(playersCards.Value.Count != 1)
+            {
+                continue;
+            }
+            
+            var card = playersCards.Value[0];
+            card.SetPower(card.GetPower() + 5);
+        }
         return true;
     }
 
     public override bool SpecialAbilityOnReveal(GameController game)
     {
-        return true;
+        return false;
+    }
+
+    public void RegisterSpecialAbilityOnGoing(GameController game)
+    {
+        if (this.GetLocationStatus() == LocationStatus.Revealed)
+        {
+            game.OnGoingLocationAbilityCall += SpecialAbilityOnGoing;
+        }
     }
 }
