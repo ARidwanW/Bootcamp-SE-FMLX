31011b58c4243cd77260f61c88a07b2b5d8daa98
ARidwanW <alwandiaridwan@gmail.com>
2023-12-12 15:40:45 +0700

    Update: implement location ability KunLun

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/KunLun.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Location/KunLun.cs
@@ -4,17 +4,30 @@ namespace MarvelSnapProject.Component.Location;
 
 public class KunLun : AbstractLocation
 {
-    public KunLun() : base(7, "K'un-Lun", "When a card moves here, give it +2 Power.", LocationAbility.KunLun, LocationStatus.Hidden, true, false)
+    public KunLun() : base(7, "K'un-Lun", "When a card moves here, give it +2 Power.", 
+                        LocationAbility.KunLun, LocationStatus.Hidden, true, false)
     {
     }
 
     public override bool SpecialAbilityOnGoing(GameController game)
     {
+        foreach(var card in GetAllCards())
+        {
+            card.SetPower(card.GetPower() + 2);
+        }
         return true;
     }
 
     public override bool SpecialAbilityOnReveal(GameController game)
     {
         return false;
+    }
+
+    public void RegisterSpecialAbilityOnGoing(GameController game)
+    {
+        if (this.GetLocationStatus() == LocationStatus.Revealed)
+        {
+            game.OnGoingLocationAbilityCall += SpecialAbilityOnGoing;
+        }
     }
 }
