fb69c87dade5dfc482b66de010010f14779a2bd4
ARidwanW <alwandiaridwan@gmail.com>
2023-12-12 08:20:15 +0700

    Update: implement ability WhiteTiger

--- IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/WhiteTiger.cs
+++ IndividualProject/MarvelSnapProject/MarvelSnapProject/Component/Card/WhiteTiger.cs
@@ -6,6 +6,9 @@ namespace MarvelSnapProject.Component.Card;
 
 public class WhiteTiger : AbstractCard
 {
+    private int _roundDeployed;
+    private AbstractLocation _locationDeployed;
+    private IPlayer _deployer;
     public WhiteTiger() : base(16, "White Tiger", "On Reveal: Add a 8-Power Tiger to another location", 
                                 5, 1, CardAbility.OnReveal, CardStatus.None, false, true)
     {
@@ -23,6 +26,82 @@ public class WhiteTiger : AbstractCard
 
     public override bool SpecialAbilityOnReveal(GameController game)
     {
+        if (IsDeployed())
+        {
+            //* NextRound --> invoke --> round + 1
+            if (game.GetCurrentRound() != _roundDeployed + 1)
+            {
+                return false;
+            }
+
+            var location = game.GetLocation(_locationDeployed);
+            var allDeployedLocation = game.GetAllDeployedLocations();
+            foreach (var deployedLocation in allDeployedLocation)
+            {
+                if(deployedLocation == location)
+                {
+                    continue;
+                }
+                var cloneWhiteTiger = this.Clone();
+                cloneWhiteTiger.SetPower(8);
+                return deployedLocation.AssignCardToLocation(cloneWhiteTiger);
+            }
+        }
+        return false;
+    }
+
+    public override bool DeployCard(GameController game, IPlayer player, AbstractLocation location)
+    {
+        if (!IsDeployed() && GetCardStatus() == CardStatus.OnHand)
+        {
+            SetCardStatus(CardStatus.OnLocation);
+            SetRoundDeployed(game.GetCurrentRound());
+            SetDeployer(player);
+            SetLocationDeployed(location);
+            RegisterSpecialAbilityOnReveal(game);
+            return true;
+        }
+        return false;
+    }
+
+    public int GetRoundDeployed()
+    {
+        return _roundDeployed;
+    }
+
+    public bool SetRoundDeployed(int round)
+    {
+        _roundDeployed = round;
         return true;
+    }
+
+    public AbstractLocation GetLocationDeployed()
+    {
+        return _locationDeployed;
+    }
+
+    public bool SetLocationDeployed(AbstractLocation location)
+    {
+        _locationDeployed = location;
+        return true;
+    }
+
+    public IPlayer GetDeployer()
+    {
+        return _deployer;
+    }
+
+    public bool SetDeployer(IPlayer player)
+    {
+        _deployer = player;
+        return true;
+    }
+
+    public void RegisterSpecialAbilityOnReveal(GameController game)
+    {
+        if (IsDeployed())
+        {
+            game.OnRevealCardAbilityCall += SpecialAbilityOnReveal;
+        }
     }
 }
