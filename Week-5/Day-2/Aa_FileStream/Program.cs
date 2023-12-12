500b8cdfed0998b92bd3c3e140512a653ff606b9
ARidwanW <alwandiaridwan@gmail.com>
2023-12-12 10:42:22 +0700

    Add: additional test file handling

--- Week-5/Day-2/Aa_FileStream/Program.cs
+++ Week-5/Day-2/Aa_FileStream/Program.cs
@@ -2,32 +2,32 @@
 
 class Program
 {
-    // static async void Main(string[] args)
-    // {
-    //     string FilePath = @".\MyFile.txt";
-    //     using (FileStream fileStream = new FileStream(FilePath,
-    //     FileMode.Create, FileAccess.Write, FileShare.None))
-    //     {
-    //         byte[] toWrite = Encoding.UTF8.GetBytes("Ini isinya");
-    //         Console.Write($"File has been opened and the Path is {FilePath}");
-    //         fileStream.Write(toWrite, 0, toWrite.Length);
-    //         Console.ReadLine();
-    //     }
-    // }
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
         string FilePath = @".\MyFile.txt";
         using (FileStream fileStream = new FileStream(FilePath,
-        FileMode.Create, FileAccess.Write, (FileShare.Delete | FileShare.ReadWrite)))
+        FileMode.Create, FileAccess.Write, FileShare.None))
         {
             byte[] toWrite = Encoding.UTF8.GetBytes("Ini isinya");
             Console.Write($"File has been opened and the Path is {FilePath}");
             fileStream.Write(toWrite, 0, toWrite.Length);
-            //fileStream.Flush();
-            await Task.Delay(100);
-            Console.WriteLine("\n finish delay");
-            fileStream.Write(toWrite, 0, toWrite.Length);
             Console.ReadLine();
         }
     }
+    // static async Task Main(string[] args)
+    // {
+    //     string FilePath = @".\MyFile.txt";
+    //     using (FileStream fileStream = new FileStream(FilePath,
+    //     FileMode.Create, FileAccess.Write, (FileShare.Delete | FileShare.ReadWrite)))
+    //     {
+    //         byte[] toWrite = Encoding.UTF8.GetBytes("Ini isinya");
+    //         Console.Write($"File has been opened and the Path is {FilePath}");
+    //         fileStream.Write(toWrite, 0, toWrite.Length);
+    //         //fileStream.Flush();
+    //         await Task.Delay(100);
+    //         Console.WriteLine("\n finish delay");
+    //         fileStream.Write(toWrite, 0, toWrite.Length);
+    //         Console.ReadLine();
+    //     }
+    // }
 }
