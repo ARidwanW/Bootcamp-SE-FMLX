23d17535adb7bf9ce16d082b3fd5d5917de0f007
ARidwanW <alwandiaridwan@gmail.com>
2023-12-20 13:52:51 +0700

    Add: init depedency injection

---
+++ Week-6/Day-3/07_DepedencyInjection/Program.cs
@@ -0,0 +1,52 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
+
+class GameController
+{
+    private IBoard _board;
+
+    public GameController(IBoard board)
+    {
+        _board = board;
+    }
+
+    public IBoard GetBoard()
+    {
+        return _board;
+    }
+}
+
+interface IBoard
+{
+    int x { get; }
+    int y { get; }
+}
+
+class Board : IBoard
+{
+    public int x {get; private set;}
+
+    public int y {get; private set;}
+
+    public Board()
+    {
+        x = 5;
+        y = 6;
+    }
+
+    public void SetX(int x)
+    {
+        this.x = x;
+    }
+
+    public void SetY(int y)
+    {
+        this.y = y;
+    }
+}
